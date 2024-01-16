b7432baa6f71b5fde9bf6fddcd013cfc5aa09cfa
Hanss Espinoza <hanssabinadie@gmail.com>
2024-01-15 22:35:19 -0600

    feat: add form control

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Task } from '../../models/task.model';
 import { TaskService } from '../../services/task.service';
@@ -7,42 +8,34 @@ import { TaskService } from '../../services/task.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required
+    ],
+  })
+
+  color = signal('red');
 
   tasksService = inject(TaskService);
 
-  tasks = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'Instalar Angular CLI',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Crear Proyecto con ng new',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Crear componente con ng g c',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Crear servicio con ng g s',
-      completed: false
-    },
-  ])
+  changeHandler():void {
+    // const input = event.target as HTMLInputElement;
+    // const newTask = input.value;
 
-  changeHandler(event: Event):void {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-
-    this.addTask(newTask);
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+        this.color.set('blue');
+      }
+    }
   }
 
   addTask(title: string): void {
@@ -51,9 +44,11 @@ export class HomeComponent {
 
   deleteTask(index: number): void {
     this.tasksService.deleteTask(index);
+    this.color.set('#000000');
   }
 
   updateTask(index: number): void {
     this.tasksService.updateTask(index);
+    this.color.set('#8E44AD');
   }
 }
